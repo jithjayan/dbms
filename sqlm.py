1a8231b760035bc8eadb456fffa93a8f182c5f79
jithjayan <jithjayan9341@gmail.com>
2024-10-16 09:28:17 +0530

    table merge

--- sqlm.py
+++ sqlm.py
@@ -5,8 +5,46 @@ cur=con.cursor()
 con.autocommit=True
 # cur.execute("create database data7")
 # cur.execute("create table std (roll int,name text,age int)")
-roll=int(input("enter roll"))
-name=str(input("enter name"))
-age=int(input("enter age"))
-cur.execute("insert into std(roll,name,age)values(%s,%s,%s)",(roll,name,age))
 
+# try:
+#     cur.execute("create table std (roll int,name text,age int)")
+# except:
+#     pass
+
+# cur.execute("insert into std (roll,name,age)values(1,'akhil',22),(2,'avis',22),(3,'dq',22),(4,'aro',22),(5,'akhil',19)")
+# roll=int(input('enter your roll number : '))
+# name=input('enter your name : ')
+# age=int(input('enter your age : '))
+
+# cur.execute("insert into std (roll,name,age)values(%s,%s,%s)",(roll,name,age))
+
+# cur.execute("select * from  std")
+# data=cur.fetchall()
+# for i in data:
+#     print(i)
+
+# cur.execute("select * from std where name like 'a%'")
+# data=cur.fetchall()
+# for i in data:
+#     print(i)
+
+# cur.execute("select * from std where name like 'i'")
+# data=cur.fetchall()
+# for i in data:
+#     print(i)
+
+
+# cur.execute("create table mark (roll int,sub text,mark int)")
+
+# cur.execute("insert into mark(roll,sub,mark)values(1,'cs',70),(1,'chem',49),(2,'phy',58),(4,'eng',45)")
+
+# cur.execute("select std.roll,std.name,std.age,mark.sub,mark.mark from std cross join mark")
+# data=cur.fetchall()
+# for i in data:
+#     print(i)
+
+
+# cur.execute("select name,count(age) from std group by name")
+# data=cur.fetchall()
+# for i in data:
+#     print(i)
